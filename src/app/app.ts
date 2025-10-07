1c29654616499f30d5cbfcd69aaf7091a8dc4a01
talles.rosa <talles.rosa@cwi.com.br>
2025-10-07 09:26:39 -0300

    Criação inicial do projeto angular, e criação de service que faz requisição http

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Movie } from './services/movie';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit  {
+
   protected readonly title = signal('frontend-curser');
+  protected listOfItems =  [
+          { title: 'Teste Explore teste the Docs', link: 'https://angular.dev' },
+          { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+          { title: 'Prompt and best practices for AI', link: 'https://angular.dev/ai/develop-with-ai'},
+          { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+          { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
+          { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+        ]
+
+   users: any[] = [];
+
+  constructor(private userService: Movie) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
+
+  printUsers (){
+    console.log(this.users)
+  }
 }
---
+++ src/app/services/movie.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+
+})
+export class Movie {
+   private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  createUser(user: any): Observable<any> {
+    return this.http.post(this.apiUrl, user);
+  }
+}
